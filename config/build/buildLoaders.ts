0d4b564b5e484fb7aa5bbc1a0bc95838dc27f421
artem_bakhmatov <ip.bakhmatov@mail.ru>
2025-02-16 16:26:07 +0300

    4. Подключаем React и настраиваем css в webpack

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,19 @@
 import webpack from "webpack"
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+	const cssLoader = {
+		test: /\.s[ac]ss$/i,
+		use: [
+			// Creates `style` nodes from JS strings
+			"style-loader",
+			// Translates CSS into CommonJS
+			"css-loader",
+			// Compiles Sass to CSS
+			"sass-loader",
+		],
+	};
+
+	// если не используем typeScript - нужен babel-loader
 	const typescriptLoader = {
 		test: /\.tsx?$/, // будет обрабатывать файлы ts и tsx (https://regex101.com)
 		use: 'ts-loader', // даёт возможность чтобы работал ts
@@ -9,5 +22,6 @@ export function buildLoaders(): webpack.RuleSetRule[] {
 
   return [ // обрабатывают файлы за рамки js, то есть ts, img и т.д.
 		typescriptLoader, 
+		cssLoader
 	]
 }
